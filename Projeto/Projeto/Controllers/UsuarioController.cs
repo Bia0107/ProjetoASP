3de32f3882488f6ff6c3d07698ec1fb7a65e0094
Bia0107 <anabsilva0107@gmail.com>
2026-03-10 17:51:34 -0300

    começando/arrumando projeto

---
+++ Projeto/Projeto/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto.Models;
+using Projeto.Repositorio.Contrato;
+
+namespace Projeto.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private IUsuarioRepositorio _usuarioRepositorio;
+
+        public UsuarioController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        [HttpGet]
+        public IActionResult CadastrarUsuario()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CadastrarUsuario(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _usuarioRepositorio.Cadastrar(usuario);
+            }
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projeto/Projeto/Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,54 @@
+﻿using MySql.Data.MySqlClient;
+using Projeto.Models;
+using Projeto.Repositorio.Contrato;
+
+namespace Projeto.Repositorio
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly string _conexaoMySQL;
+
+        public UsuarioRepositorio(IConfiguration configuration)
+        {
+            _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+        }
+        public void Atualizar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Usuario usuario)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("insert into tbUsuario(NomeUsu, Cargo, DataNasc" +
+                                                     "values (@NomeUsu, @Cargo, @DataNasc)", conexao);
+
+                cmd.Parameters.Add("@NomeUsu", MySqlDbType.VarChar).Value = usuario.NomeUsu;
+                cmd.Parameters.Add("@Cargo", MySqlDbType.VarChar).Value = usuario.Cargo;
+                cmd.Parameters.Add("@DataNasc", MySqlDbType.VarChar).Value = usuario.DataNasc;
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+
+        }
+
+        public void Excluir(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Usuario> ObterTodosUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario ObterUsuario(int Id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
